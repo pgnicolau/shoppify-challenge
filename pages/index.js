175e34b356d61a8e5f93d8ef185ababf116f691e
Gabriela Nicolau <gabriella.nicolau@gmail.com>
2021-01-16 17:09:27 -0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,89 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import * as React from "react";
+import {useEffect, useState} from "react";
+import useSWR from "swr";
+
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    const [query, setQuery] = useState('');
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+    const {data, error} = useSWR(`http://www.omdbapi.com/?apikey=d1461bf&s=${query}`, fetch)
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+    const [searchResult, setSearchResult] = useState(data)
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    console.log(searchResult.body)
+    console.log(data)
+    console.log(error)
+
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Shoppies Movie Awards </title>
+                <link rel="icon" href="/gn-logo.png"/>
+            </Head>
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    The Shoppies Movie Awards
+                </h1>
+
+                <br/><br/>
+
+                <div className={styles.card}>
+                    <h3>Search by Movie Title</h3>
+                    <div className={styles.inputContainer}>
+                        <input className={styles.searchbar} placeholder="Search..."
+                               onChange={({target}) => setQuery(target.value)}
+
+                        />
+                        <img className={styles.searchIcon} src="/search.png" alt="search icon"/>
+                    </div>
+                </div>
+
+                <div className={styles.grid}>
+                    <div className={styles.card}>
+                        <h3>Results for "{query}"</h3>
+                        <ul>
+                            {data ?
+                                // data.map(movie =>
+                                    <a>
+                                        <li className={styles.list}><img className={styles.nomIcon} src="/like.png"
+                                                                         alt="nominate"/> tea
+                                        </li>
+                                    </a>
+                                // )
+
+                                : ""}
+                        </ul>
+                    </div>
+
+                    <div className={styles.card}>
+                        <h3>Nominations</h3>
+                        <ul>
+                            <a>
+                                <li className={styles.list}><img className={styles.nomIcon} src="/like.png"
+                                                                 alt="nominate"/> Tea
+                                </li>
+                            </a>
+                        </ul>
+                    </div>
+                </div>
+            </main>
+
+            <footer className={styles.footer}>
+                <a
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    With &hearts; by{' '}
+                    <img src="/gn-logo.png" alt="GN Logo" className={styles.logo}/>
+                </a>
+            </footer>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    )
 }
